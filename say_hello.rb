cc43858fd1b5382707ee0407bf89abf7bb52ca58
Harrison Price <hprice518@gmail.com>
2020-04-15 15:54:34 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,20 @@
 # Build your say_hello method here
+
+def say_hello(name)
+  puts "Hello #{name}!"
+end
+
+say_hello("Gabriela")
+
+  def say_hello(name = "Ruby programmer")
+    puts "Hello #{name}!"
+  end
+
+  say_hello
+  
+  def say_hello(name = "Ruby Programmer")
+    puts "Hello #{name}!"
+  end
+  
+  say_hello("Gabriela")
+  
